e82208c837861ebf49a0eb838a9303523c32b4d7
coolman <zhanglianxin1993@163.com>
2017-10-24 11:49:37 +0800

    The column length is tooooo small :pensive:

--- data/scripts/schema.mysql.sql
+++ data/scripts/schema.mysql.sql
@@ -8,6 +8,6 @@ CREATE TABLE book (
     id int(11) NOT NULL AUTO_INCREMENT,
     author VARCHAR(32) NOT NULL,
     title VARCHAR(32) NOT NULL,
-    imagepath VARCHAR(32) NOT NULL,
+    imagepath VARCHAR(64) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT "";
