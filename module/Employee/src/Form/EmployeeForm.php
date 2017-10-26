8b2467b87e138894563d06c49aeceb576cdd6c60
coolman <zhanglianxin1993@163.com>
2017-10-26 18:01:35 +0800

    Complete the view script for adding a new record

--- module/Employee/src/Form/EmployeeForm.php
+++ module/Employee/src/Form/EmployeeForm.php
@@ -9,32 +9,32 @@ class EmployeeForm extends Form
     public function __construct($name = null)
     {
         parent::__construct('employee');
-    }
 
-    $this->add([
-        'name' => 'id',
-        'type' => 'hidden',
-    ]);
-    $this->add([
-        'name' => 'emp_name',
-        'type' => 'text',
-        'options' => [
-            'label' => 'Name',
-        ],
-    ]);
-    $this->add([
-        'name' => 'emp_job',
-        'type' => 'text',
-        'options' => [
-            'label' => 'Job',
-        ],
-    ]);
-    $this->add([
-        'name' => 'submit',
-        'type' => 'submit',
-        'attributes' => [
-            'value' => 'Go',
-            'id' => 'submitbutton',
-        ],
-    ]);
+        $this->add([
+            'name' => 'id',
+            'type' => 'hidden',
+        ]);
+        $this->add([
+            'name' => 'emp_name',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Name',
+            ],
+        ]);
+        $this->add([
+            'name' => 'emp_job',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Job',
+            ],
+        ]);
+        $this->add([
+            'name' => 'submit',
+            'type' => 'submit',
+            'attributes' => [
+                'value' => 'Go',
+                'id' => 'submitbutton',
+            ],
+        ]);
+    }
 }
